9780ed2cfe9dd39cd7c2bac8dfce62ee8aaaf821
Krystof Rainisch <Krystof.Rainisch@hey.com>
2022-06-29 00:21:27 +0200

    remove express

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,22 @@
 import 'dotenv/config'
-import * as express from 'express'
-import * as http from 'http'
-import bodyParser from 'body-parser'
 
 import App from './App'
 import TwitterClient from './Twitter'
 
 const myApp = new App()
-myApp.initialize().then(() => {
-    const app = express.default()
 
-    app.use(bodyParser.json())
-
-    app.get('/', async (_req, res) => {
-        res.send({
+myApp.initialize().then(async () => {
+    try {
+        const { prediction } = await myApp.generateStoryWithOpenAI()
+        const tweet = `${prediction}`
+        const result = await TwitterClient.v2.tweet(tweet)
+        console.log({
             uptime: process.uptime(),
-            ...myApp.generateStory(),
+            tweet,
+            tweetId: result.data.id,
         })
-    })
-    app.get('/ai', async (_req, res) => {
-        res.send({
-            uptime: process.uptime(),
-            ...(await myApp.generateStoryWithOpenAI()),
-        })
-    })
-    app.get('/tweet', async (req, res) => {
-        try {
-            const { prediction } = await myApp.generateStoryWithOpenAI()
-            const tweet = `${prediction}`
-            const result = await TwitterClient.v2.tweet(tweet)
-            res.send({
-                uptime: process.uptime(),
-                tweet,
-                tweetId: result.data.id,
-            })
-        } catch (e) {
-            console.error(e)
-            throw new Error('Failed to tweet')
-        }
-    })
-
-    const server = http.createServer(app)
-
-    server.listen(4040, () => {
-        console.info('Server started')
-    })
+    } catch (e) {
+        console.error(e)
+        throw new Error('Failed to tweet')
+    }
 })
